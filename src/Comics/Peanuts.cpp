a5eacb78978e232528981e99ae2db44b1e0a050d
damien <damien@tardypad.me>
2015-10-12 01:07:38 +0200

    Update cover of Peanuts comic
    https://pmcdeadline2.files.wordpress.com/2013/01/peanuts__130108130109.jpg

--- src/Comics/Peanuts.cpp
+++ src/Comics/Peanuts.cpp
@@ -16,7 +16,7 @@ Peanuts::Peanuts(QObject *parent) :
 {
     m_info.id             = QString("peanuts");
     m_info.name           = QString("Peanuts");
-    m_info.color          = QColor(133, 248, 255);
+    m_info.color          = QColor(255, 223, 30);
     m_info.authors        = QStringList("Charles Monroe Schulz");
     m_info.homepage       = QUrl("http://www.peanuts.com/");
     m_info.country        = QLocale::UnitedStates;
