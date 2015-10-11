57a753a94ba335b2c3be531cc5e9b9c0631dcd85
damien <damien@tardypad.me>
2015-10-12 00:55:20 +0200

    Update cover of Foxtrot comic
    https://thecomicninja.files.wordpress.com/2012/10/foxtrot-tonights-the-night.jpg

--- src/Comics/FoxTrot.cpp
+++ src/Comics/FoxTrot.cpp
@@ -16,7 +16,7 @@ FoxTrot::FoxTrot(QObject *parent) :
 {
     m_info.id             = QString("foxtrot");
     m_info.name           = QString("FoxTrot");
-    m_info.color          = QColor(222, 196, 169);
+    m_info.color          = QColor(250, 203, 123);
     m_info.authors        = QStringList("Bill Amend");
     m_info.homepage       = QUrl("http://www.foxtrot.com");
     m_info.country        = QLocale::UnitedStates;
