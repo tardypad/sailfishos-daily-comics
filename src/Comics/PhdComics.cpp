ff49ed57195e66df0a4d02a9e4503c84a0432104
damien <damien@tardypad.me>
2015-10-12 01:09:29 +0200

    Update cover of PHD Comics comic
    http://www.userlogos.org/files/logos/inductiveload/PhD%20Comics.png
    https://wishihadgills.files.wordpress.com/2010/09/phd090110s.gif
    http://carletonnow.carleton.ca/ccms/wp-content/uploads/comic2.jpg

--- src/Comics/PhdComics.cpp
+++ src/Comics/PhdComics.cpp
@@ -16,7 +16,7 @@ PhdComics::PhdComics(QObject *parent) :
 {
     m_info.id             = QString("phdcomics");
     m_info.name           = QString("Piled Higher and Deeper");
-    m_info.color          = QColor(82, 105, 125);
+    m_info.color          = QColor(82, 104, 125);
     m_info.authors        = QStringList("Jorge Cham");
     m_info.homepage       = QUrl("http://phdcomics.com/");
     m_info.country        = QLocale::Panama;
