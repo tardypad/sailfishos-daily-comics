tardypad/sailfishos-daily-comics

commit 87c880a679bef3bb824cff2b0fc115a0f81bd98d

fix Error spelling in progress bar if no error

qml/utils/ProgressInfoBar.qml
@@ -87,7 +87,7 @@ Rectangle {
                     left: errorImage.right
                     leftMargin: Theme.paddingSmall
                 }
-                text: errorCount > 1 ? errorCount + " Errors" : "1 Error"
+                text: errorCount + (errorCount > 1 ?  + " Errors" : " Error")
                 font.pixelSize: Theme.fontSizeExtraSmall
             }
         }

commit f0801ac8892cb858c7f1a5ffcc797e8944b2e5ce

fix typo error display in progress info bar
introduced in 87c880a679bef3bb824cff2b0fc115a0f81bd98d

qml/utils/ProgressInfoBar.qml
@@ -94,7 +94,7 @@ Rectangle {
                     left: errorImage.right
                     leftMargin: Theme.paddingSmall
                 }
-                text: errorCount + (errorCount > 1 ?  + " Errors" : " Error")
+                text: errorCount + (errorCount > 1 ? " Errors" : " Error")
                 font.pixelSize: Theme.fontSizeExtraSmall
             }
         }