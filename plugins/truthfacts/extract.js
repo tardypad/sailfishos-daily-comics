70686ae0278cd553ca7838397cfcab0be0f3fed5
Damien Tardy-Panis <damien@tardypad.me>
2016-12-05 20:18:41 +0100

    fix Truth Facts comic

--- plugins/truthfacts/extract.js
+++ plugins/truthfacts/extract.js
@@ -1,5 +1,5 @@
 function(page) {
-    var regex = /<img[^>]*src="([^"]*\/img\/truth_facts\/[^"]*)"/;
+    var regex = /<img[^>]*data-src="([^"]*\/static\/[^"]*)"/;
     var match = regex.exec(page);
     return match[1];
 }
