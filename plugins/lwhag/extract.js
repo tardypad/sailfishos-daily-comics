6096b5207205f54681540db8fb49c7674fd4f337
Damien Tardy-Panis <damien@tardypad.me>
2017-10-03 23:56:51 +0200

    fix Living with Hipstergirl and Gamergirl comic
    add fallback regex

--- plugins/lwhag/extract.js
+++ plugins/lwhag/extract.js
@@ -1,5 +1,12 @@
 function(page) {
-    var regex = /<img[^>]*src="([^"]*english[^"]*)"/;
-    var match = regex.exec(page);
-    return match[1];
+    var regex1 = /<img[^>]*src="([^"]*english[^"]*)"/;
+    var match1 = regex1.exec(page);
+
+    if (match1 !== null) {
+        return match1[1];
+    }
+
+    var regex2 = /<img[^>]*src="([^"]*en\.[^"]*)"/;
+    var match2 = regex2.exec(page);
+    return match2[1];
 }
