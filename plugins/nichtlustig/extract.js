70b69a4505ba996549f28a43437ddbeece05850c
Damien Tardy-Panis <damien@tardypad.me>
2017-10-04 23:51:00 +0200

    remove discontinued comics
    Thanks for them:
    - The Adventures of Dr. McNinja
    - The Hat and Fat
    - Nichtlustig
    - Octopus Pie
    - Rabbitual Offender
    - Sad Sacks
    - Tubey Toons
    
    Some other are discontinued too but they still have a place that
    present a different archive comic regularly

--- plugins/nichtlustig/extract.js
+++
@@ -1,5 +0,0 @@
-function(page) {
-    var regex = /<link rel="image_src" href="([^"]*)"/;
-    var match = regex.exec(page);
-    return match[1];
-}
--- plugins/rabbitualoffender/extract.js
+++
@@ -1,5 +0,0 @@
-function(page) {
-    var regex = /&lt;img[^&]*src="([^"]*media.tumblr.com\/[^"]*)"/;
-    var match = regex.exec(page);
-    return match[1];
-}
