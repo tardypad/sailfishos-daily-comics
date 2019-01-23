32ac0a962ff92310807e7317d329723fc3efa8f7
Damien Tardy-Panis <damien@tardypad.me>
2019-01-23 23:27:01 +0100

    add curl to user agent
    seems to prevent Tumblr from redirecting to the privacy consent page

--- src/Comic.cpp
+++ src/Comic.cpp
@@ -119,7 +119,7 @@ void Comic::fetchStripSource(QUrl stripSrcUrl)
 
     QUrl requestUrl = !stripSrcUrl.isEmpty() ? stripSrcUrl : stripSourceUrl();
     QNetworkRequest request(requestUrl);
-    request.setHeader(QNetworkRequest::UserAgentHeader, "sailfishos/tardypad/dailycomics");
+    request.setHeader(QNetworkRequest::UserAgentHeader, "sailfishos/tardypad/dailycomics curl");
     m_currentReply = m_networkManager->get(request);
 
     m_timeoutTimer->start();
@@ -220,7 +220,7 @@ void Comic::fetchStripImage(QUrl stripImageUrl)
     m_currentReply = NULL;
 
     QNetworkRequest request(stripImageUrl);
-    request.setHeader(QNetworkRequest::UserAgentHeader, "sailfishos/tardypad/dailycomics");
+    request.setHeader(QNetworkRequest::UserAgentHeader, "sailfishos/tardypad/dailycomics curl");
     m_currentReply = m_networkManager->get(request);
 
     m_timeoutTimer->start();
