38521e6a41e795bac5485fdbcc84cb72025eb31a
Damien Tardy-Panis <damien@tardypad.me>
2019-01-23 23:31:47 +0100

    use higher resolution images for some Tumblr comics

--- plugins/lizclimo/extract.js
+++ plugins/lizclimo/extract.js
@@ -1,5 +1,5 @@
 function(page) {
     var regex = /&lt;img[^&]*src="([^"]*media.tumblr.com\/[^"]*)"/;
     var match = regex.exec(page);
-    return match[1];
+    return match[1].replace("_500","_1280");
 }
