5d4d49858407f1401a0a6870975c80767bee18db
Rob K <git@cow-n-berg.nl>
2019-05-11 07:40:01 +0200

    Hein de Kort complete update
    Extract working again, and artwork changed as suggested by Tardypad

--- plugins/heindekort/extract.js
+++ plugins/heindekort/extract.js
@@ -1,5 +1,5 @@
 function(page) {
-    var regex = /<figure\sitemscope="itemscope"\sitemtype="http:\/\/schema.org\/ImageObject"\sitemprop="image"\sclass="fjs-gallery-item"\sdata-hires="(https:\/\/[^"]*)"/;
+    var regex = /<img[^>]*src="([^"]*)"/;
     var match = regex.exec(page);
-    return match[1];
+    return match[1].replace(/&amp;/g, '&');
 }
