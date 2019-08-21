babc88c188c437234cb373a313fc7517e36d2f64
Oleg Linkin <MaledictusDeMagog@gmail.com>
2019-08-22 00:36:30 +0300

    Use ComicsInfoUpdater in FavoriteComicsModel

--- src/FavoriteComicsModel.cpp
+++ src/FavoriteComicsModel.cpp
@@ -8,16 +8,24 @@
 #include "FavoriteComicsModel.h"
 
 #include <QDebug>
+#include <QJsonArray>
+#include <QJsonObject>
+#include <QJsonValue>
 
 #include "ComicFactory.h"
 #include "Comic.h"
 #include "ComicDatabaseResource.h"
+#include "ComicsInfoUpdater.h"
 #include "Settings.h"
 
 FavoriteComicsModel::FavoriteComicsModel(QObject *parent) :
-    ComicsModel(parent)
+    ComicsModel(parent),
+    m_comicsInfoUpdater(new ComicsInfoUpdater(this))
 {
     connect(settings, &Settings::favoritesChanged, this, &FavoriteComicsModel::favoritesChanged);
+
+    connect(m_comicsInfoUpdater.data(), &ComicsInfoUpdater::gotUpdateInfo,
+            this, &FavoriteComicsModel::handleGotUpdateInfo);
 }
 
 QStringList FavoriteComicsModel::idLoadList()
@@ -37,6 +45,48 @@ QStringList FavoriteComicsModel::idLoadList()
     return loadList;
 }
 
+void FavoriteComicsModel::updateComics(const QJsonDocument& updateInfo)
+{
+    QJsonObject object = updateInfo.object();
+    if (!object.contains("plugins") || !object["plugins"].isArray())
+    {
+        return;
+    }
+
+    QJsonArray array = object["plugins"].toArray();
+    for (const QJsonValue& value : array)
+    {
+        if (!value.isObject())
+        {
+            continue;
+        }
+
+        QJsonObject update = value.toObject();
+        if (!update.contains("id"))
+        {
+            continue;
+        }
+        const QString id = update["id"].toString();
+        auto it = std::find_if(m_list.begin(), m_list.end(), [id](decltype(m_list.front()) item)
+            { return item->id() == id; });
+        if (it == m_list.end())
+        {
+            continue;
+        }
+        ComicInfo info = (*it)->getInfo();
+        if (update.contains("stripSource") && update["stripSource"].isString())
+        {
+            info.stripSourceUrl = QUrl(update["stripSource"].toString());
+        }
+        (*it)->setInfo(info);
+
+        if (update.contains("extractJson") && update["extractJson"].isString())
+        {
+            (*it)->setExtractScript(update["extractJson"].toString());
+        }
+    }
+}
+
 void FavoriteComicsModel::removeFavorite(int row)
 {
     Comic* comic = comicAt(row);
@@ -50,4 +100,18 @@ void FavoriteComicsModel::removeFavorite(int row)
     beginRemoveRows(QModelIndex(), row, row);
     m_list.removeAt(row);
     endRemoveRows();
+}
+
+void FavoriteComicsModel::fetchAll()
+{
+    m_comicsInfoUpdater->startUpdate();
+}
+
+void FavoriteComicsModel::handleGotUpdateInfo(const QJsonDocument& updateInfo)
+{
+    if (!updateInfo.isNull())
+    {
+        updateComics(updateInfo);
+    }
+    ComicsModel::fetchAll();
 }
--- src/FavoriteComicsModel.h
+++ src/FavoriteComicsModel.h
@@ -8,20 +8,32 @@
 #ifndef FAVORITECOMICSMODEL_H
 #define FAVORITECOMICSMODEL_H
 
+#include <QSharedPointer>
+
 #include "ComicsModel.h"
+
+class ComicsInfoUpdater;
 
 class FavoriteComicsModel : public ComicsModel
 {
     Q_OBJECT
+
+    QSharedPointer<ComicsInfoUpdater> m_comicsInfoUpdater;
 public:
     explicit FavoriteComicsModel(QObject *parent = 0);
     Q_INVOKABLE void removeFavorite(int row);
 
 signals:
     void favoritesChanged();
+public slots:
+    Q_INVOKABLE virtual void fetchAll() override;
+private slots:
+    void handleGotUpdateInfo(const QJsonDocument& updateInfo);
 
 protected:
-    virtual QStringList idLoadList();
+    virtual QStringList idLoadList() override;
+private:
+    void updateComics(const QJsonDocument& updateInfo);
 };
 
 #endif // FAVORITECOMICSMODEL_H
