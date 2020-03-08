fea64580a14a6df7d5fbdc72ced3858e409486fd
Oleg Linkin <oleg.linkin@idt.net>
2020-03-08 19:28:41 +0300

    Update einbilder comics

--- plugins/einbilder/extract.js
+++ plugins/einbilder/extract.js
@@ -1,5 +1,5 @@
 function(page) {
-    var regex = /<img[^>]*src="(images\/cartoons\/[^"]*)"/;
+    var regex = /<meta\sproperty="og:image"\scontent="([^"]*)"/;
     var match = regex.exec(page);
     return match[1];
 }
