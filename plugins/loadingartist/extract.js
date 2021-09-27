838980acb4853c030f9ddb2f386c42d99b5a75ce
Rob K <github@cow-n-berg.nl>
2021-09-27 21:36:41 +0200

    Swamp and Loading Artist
    Small changes

--- plugins/loadingartist/extract.js
+++ plugins/loadingartist/extract.js
@@ -1,5 +1,5 @@
 function(page) {
-    var regex = /<img[^>]*src="([^"]*\/wp-content\/uploads\/[^"]*)"/;
+    var regex = /<img src=(\/comic[^\s]*.jpg)/;
     var match = regex.exec(page);
     return match[1];
 }
