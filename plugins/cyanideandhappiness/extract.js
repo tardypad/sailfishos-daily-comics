35f5379b2af54645fbe9f21e92dbc34c790d8546
olf <Olf0@users.noreply.github.com>
2023-08-26 23:45:51 +0200

    [extract.js] Shorten RegEx slightly

--- plugins/cyanideandhappiness/extract.js
+++ plugins/cyanideandhappiness/extract.js
@@ -1,5 +1,5 @@
 function(page) {
-    var regex = /<link[^>]* as="image"[^>]* href="(http[s]?:\/\/static.explosm.net\/20[2-9][0-9]\/[0-1][0-9]\/[0-9]+\/[-+_0-9A-Za-z]+\.[-+_0-9a-z]+)"/;
+    var regex = /<link[^>]* as="image"[^>]* href="(https?:\/\/static.explosm.net\/20[2-9][0-9]\/[01][0-9]\/[0-9]+\/[-+_0-9A-Za-z]+\.[-+_0-9a-z]+)"/;
     var match = regex.exec(page);
     return match[1];
 }
