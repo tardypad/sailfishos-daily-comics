0c2a57d3552ddae5781a34983d2203a7fb8795b7
olf <Olf0@users.noreply.github.com>
2023-08-26 20:33:39 +0200

    [extract.js] Fix "Cyanide and Happiness"
    The extraction RegEx may be shortened, but a "best and longest match"-approach is used to avoid ambiguities.

--- plugins/cyanideandhappiness/extract.js
+++ plugins/cyanideandhappiness/extract.js
@@ -1,5 +1,5 @@
 function(page) {
-    var regex = /<img[^>]*src="([^"]*\/comics\/[^"]*)"/;
+    var regex = /<link rel="preload" as="image" href="(https:\/\/static.explosm.net\/20[2-9][0-9]\/[0-1][0-9]\/[0-9]+\/[a-z]+.png)"\/>/;
     var match = regex.exec(page);
     return match[1];
-}
+
