53598d112df264c12be64d57c9810036422888a6
olf <Olf0@users.noreply.github.com>
2023-08-27 04:46:14 +0200

    [extract.js] Slightly enhance RegEx

--- plugins/suporte/extract.js
+++ plugins/suporte/extract.js
@@ -1,5 +1,5 @@
 function(page) {
-    var regex = /<img[^>]* src="([^"]*\/wp-content\/uploads\/[^"]*\/Suporte_[^"]*)"/;
+    var regex = /<img[^>]* src="([^"]+\/wp-content\/uploads\/[^"]*Suporte[^"]+)"/;
     var match = regex.exec(page);
     return match[1];
 }
